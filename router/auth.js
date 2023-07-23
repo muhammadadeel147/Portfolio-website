fb63784880158198f2c2efff40371b02a1187961
Muhammad Adeel <muhammaddadeel90@gmail.com>
2023-07-23 15:18:23 +0500

    hashing and login page done

--- router/auth.js
+++ router/auth.js
@@ -10,36 +10,110 @@ router.get("/", (req, res) => {
     res.send("Welcome to websitest! router");
 });
 
-router.post("/register", (req, res) => {
-//console.log(req.body);
-//res.json({message:req.body});
+//using promises
 
-const{name,email,phone,work,password,cpassword}=req.body;
 
-if (!name || !email || !phone || !work || !password || !cpassword) {
-    return res.status(422).json({error:"Field is missing"});
-}
+// router.post("/register", (req, res) => {
+// //console.log(req.body);
+// //res.json({message:req.body});
 
-User.findOne({email:email})
-.then((userExist)=>{
-    if (userExist){
-        return res.status(422).json({error:"email already exists"});
+// const{name,email,phone,work,password,cpassword}=req.body;
+
+// if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(422).json({error:"Field is missing"});
+// }
+
+// User.findOne({email:email})
+// .then((userExist)=>{
+//     if (userExist){
+//         return res.status(422).json({error:"email already exists"});
+//     }
+
+//     const user=new User({name,email,phone,work,password,cpassword});
+
+//     user.save().then(()=>{
+//         res.status(201).json({message:"user egistered successfully"});
+//     }).catch((error)=>res.status(500).json({message:"failed to register"}));
+
+// })
+// .catch(err=>{console.log(err);});
+
+
+
+// //console.log(name);
+// //console.log(email);
+
+// });
+
+
+router.post("/register",async (req, res) => {
+    //console.log(req.body);
+    //res.json({message:req.body});
+    
+    const{name,email,phone,work,password,cpassword}=req.body;
+    
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({error:"Field is missing"});
     }
+    try {
+     
+    const userExist=  await User.findOne({email:email})
+    
+        if (userExist){
+            return res.status(422).json({error:"email already exists"});
+        }
+        else if(password != cpassword){
+            return res.status(422).json({error:"password did not match"});
+        }else{
+            const user=new User({name,email,phone,work,password,cpassword});
+            //middle ware use for hashing passwords
+              await  user.save();
+                    res.status(201).json({message:"user egistered successfully"});
+        }
+    
+      
+        
+    }
+     catch (error) {
+        console.log(err);
+    }
+  //console.log(name);
+    //console.log(email);
+    
+    });
 
-    const user=new User({name,email,phone,work,password,cpassword});
-
-    user.save().then(()=>{
-        res.status(201).json({message:"user egistered successfully"});
-    }).catch((error)=>res.status(500).json({message:"failed to register"}));
-
-})
-.catch(err=>{console.log(err);});
+// video foloow login page
 
 
 
-//console.log(name);
-//console.log(email);
 
-});
+// Self made login page
+//     router.post("/login", async (req, res) => {
+       
+//         const{email,password,}=req.body;
+    
+//         if ( !email || !password ) {
+//             return res.status(422).json({error:"No Empty Field"});
+//         }
+//         try {
+//             const userExist=  await User.findOne({email:email});
+// if(userExist) {
+//     const userPassword=userExist.password===req.body.password;
+//     if(userPassword) {
+//         res.status(201).json({message:"user Login successfully"});
+//     }
+//     else{
+//         res.status(500).json({message:"password didnot match"});
+//     }
+
+// }
+// else{
+//     res.status(500).json({message:"user does not exist"});
+// }
+//         } catch (error) {
+//             console.log(error);
+//         }
+
+//     });
 
 module.exports = router;
